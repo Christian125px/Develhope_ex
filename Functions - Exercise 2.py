f8bc245b63bbd11ce0469f43441804f79f6c8a27
Christian125px <129185666+Christian125px@users.noreply.github.com>
2023-07-23 18:37:39 +0200

    Functions - Exercise 2
    Python Exercises for Python Develhope Crash Course

---
+++ Functions - Exercise 2.py
@@ -0,0 +1,20 @@
+# Functions - Exercise 2 🐍
+
+"""
+inside the paranthesis variables can be used those variables as known as parameters of the function provides control inside the function
+example is here 
+```
+def hello(name):
+    print(f"hello {name}")
+```
+
+Now you create a function named goodbye with a parameter `name` and say "Good bye Adam" 
+"""
+
+# function definition
+def goodbye ( name ) :
+
+    print( f"Good bye {name}" )
+
+# function call
+goodbye( 'Adam' )
